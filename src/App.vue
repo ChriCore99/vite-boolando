d494cc3942d6f022191fff51e9741081bcbf15f8
ChriCore99 <christian.corelli.io@gmail.com>
2024-04-10 16:15:21 +0200

    ADD: miglioramento link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
+<script>
 import HeaderSection from './components/HeaderSection.vue'
 import MainSection from './components/MainSection.vue'
 import FooterSection from './components/FooterSection.vue'
+
+export default {
+  components: {
+    HeaderSection,
+    MainSection,
+    FooterSection,
+  }
+}
 </script>
 
 <template>
